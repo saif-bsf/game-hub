620433af949181fa911e18728ce75b63593d88f8
saif <saif.s@bsfdv.com>
2023-12-11 12:11:41 +0400

    fix: layout issue

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -10,11 +10,7 @@ const GameGrid = () => {
   return (
     <>
       {error && <Text>{error}</Text>}
-      <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
-        spacing={10}
-        p="10px"
-      >
+      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 4 }} spacing={3} p="10px">
         {isLoading &&
           skeletons.map((skeleton) => (
             <GameCardContainer>
