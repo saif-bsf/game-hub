795a8a90fe045245117efa6314370a682db90099
saif <saif.s@bsfdv.com>
2023-12-11 13:17:00 +0400

    feat: sort selector

---
+++ src/components/SortSelector.tsx
@@ -0,0 +1,22 @@
+import { Menu, MenuButton, Button, MenuList, MenuItem } from "@chakra-ui/react";
+import { BsChevronDown } from "react-icons/bs";
+
+const SortSelector = () => {
+  return (
+    <Menu>
+      <MenuButton as={Button} rightIcon={<BsChevronDown />}>
+        Order By: Relevance
+      </MenuButton>
+      <MenuList>
+        <MenuItem>Relevance</MenuItem>
+        <MenuItem>Date added</MenuItem>
+        <MenuItem>Name</MenuItem>
+        <MenuItem>Average Rating</MenuItem>
+        <MenuItem>Release Date</MenuItem>
+        <MenuItem>Popularity</MenuItem>
+      </MenuList>
+    </Menu>
+  );
+};
+
+export default SortSelector;
